51164183d5f2da74700bf0c8959276ae1a0ef95e
Stuart Maitland <stuart@mediwikis.com>
2018-05-01 16:00:28 +0100

    Updated granular data & z score via LMS method

--- grip_strength.js
+++ grip_strength.js
@@ -11,12 +11,20 @@ function get_val() {
     }
     var age = document.getElementById("age").value;
     var grip = document.getElementById("grip").value;
-    age = Math.ceil(age/5)*5
-    var mean = data[gender][age.toString()].mean;
-    var sd = data[gender][age.toString()].sd;
-    var z_score = ((grip-mean)/sd).toFixed(2);
+    age = Math.ceil(age/0.5)*0.5
+    age = age.toFixed(1).toString()
+    var median = data[gender][age].M;
+    var l = data[gender][age].L;
+    var s = data[gender][age].S;
+    var intermediate=Math.pow((grip/median),l)-1
+    var z_score = (intermediate)/(l*s)
+
+    var z_score = z_score.toFixed(2);
 
     document.getElementById("z_score_out").innerHTML = z_score;
-    document.getElementById("grip_out").innerHTML = mean + " (" + sd + ")";
+    document.getElementById("grip_out").innerHTML = median;
+    document.getElementById("l_out").innerHTML = l;
+    document.getElementById("s_out").innerHTML = s;
+
 }
 
